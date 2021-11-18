4d6123e95c8bc2baf4f8b2a733f894604fa135cc
Mickey253 <jacob_l._miller@hotmail.com>
2021-11-17 22:33:10 -0700

    Added comments

--- src/hyperbolic_functions.js
+++ src/hyperbolic_functions.js
@@ -1,7 +1,7 @@
 //Coordinate systems
 /**
  * The coordinate system transformation from canvas to poincare disk.
- * @param {NodeObject} p - The node of a graph with defined `x` and `y` in poincare disk. 
+ * @param {NodeObject} p - The node of a graph with defined `x` and `y` in the canvas.
  * @param {PoinDisk} poindisk - The poindisk object to use.
  * @returns {{x: number, y: number}} - The new position in the canvas of poindisk.
  */
@@ -21,6 +21,15 @@ function canvas_to_disk(p, poindisk){
 }
 exports.canvas_to_disk = canvas_to_disk;
 
+/**
+* Projects points from the Euclidean plane to the poincare disk
+*@param {NodeObject} ePosition - Point with defined x and y in the canvas
+*@param {float} centerX - x coordinate of geometric mean of data
+*@param {float} centerY - y coordinate of geometric mean of data
+*@param {PoinDisk} poindisk - The poindisk object to use
+*@param {boolean} inPlace - Whether the data is already in place or not.
+*@returns {circle: {cx: number, cy: number, r: number}}
+*/
 exports.to_poincare = function to_poincare(ePosition, centerX, centerY, poindisk, inPlace = true) {
   //ePosition: position of node on canvas
   //centerX,centerY: geometric mean of data
@@ -53,27 +62,58 @@ exports.to_poincare = function to_poincare(ePosition, centerX, centerY, poindisk
   }
 }
 
+
+/**
+ * The coordinate system transformation from canvas to poincare disk.
+ * @param {NodeObject} p - A point with defined `x` and `y` in terms of the poincare disk.
+ * @param {PoinDisk} poindisk - The poindisk object to use.
+ * @returns {'x': number, 'y': number} - New position in canvas of p
+ */
 function disk_to_canvas(p, poindisk){
   let x = p.x*poindisk.r + poindisk.cx;
   let y = -p.y*poindisk.r + poindisk.cy;
   return {'x': x, 'y': y};
 }
 
+
+/**
+ * Convert polar coordinates to cartesian coordinates.
+ * @param {number} r - Radial component of polar coordinate
+ * @param {number} theta - Angular component of polar coordinate
+ * @param {NodeObject} center - center from which r and theta are calculated
+ * @returns {'x': number, 'y': number}  - cartesian coordiates of point
+ */
 function polar_to_cart(r,theta,center={'x':0,'y':0}){
   return{'x': center.x + (r*Math.cos(theta)), 'y': center.y + (r*Math.sin(theta))};
 }
 
+/**
+ * Convert cartesian coordinates to polar coordinates.
+ * @param {NodeObject} p - A point with defined 'x' and 'y'
+ * @returns {'r': number, 'theta': number}  - polar coordinates of point
+ */
 function cart_to_polar(p){
   let r = Math.sqrt(p.x*p.x+p.y*p.y);
   let theta = Math.atan2(p.y,p.x);
   return {'r': r, "theta": theta};
 }
 
-//Distances
+/**
+ * Euclidean Distance.
+ * @param {NodeObject} p - A point with defined 'x' and 'y'
+ * @param {NodeObject} q - A point with defined 'x' and 'y'
+ * @returns {number}  - Euclidean distance between p and q
+ */
 function euclid_dist(p,q){
   return Math.sqrt(Math.pow(p.x-q.x,2) + Math.pow(p.y-q.y,2));
 }
 
+/**
+ * Hyperbolic Distance.
+ * @param {NodeObject} p - A point with defined 'x' and 'y'
+ * @param {NodeObject} q - A point with defined 'x' and 'y'
+ * @returns {number}  - Hyperbolic distance between p and q
+ */
 function hyper_dist(p,q){
   let pow = Math.pow;
   let numerator = 2*(pow(p.x-q.x,2)+pow(p.y-q.y,2));
@@ -82,17 +122,32 @@ function hyper_dist(p,q){
   return Math.acosh(1 + (numerator/denominator));
 }
 
+/**
+ * Returns true distance from orgin given the distance in the projection.
+ * @param {number} r - Poincare radius
+ * @returns {number}  - True distance from origin
+ */
 function r_poincare_to_euclid(r){
   return Math.tanh(r/2);
 }
 
+/**
+ * Returns Poincare distance from orgin given the true hyperbolic distance.
+ * @param {number} r - Hyperbolic radius
+ * @returns {number}  - Poincare distance from origin
+ */
 function hyper_radius_from_euclidean(r){
   return 2*Math.atanh(r)
 }
 
-//Euclidean lines
+/**
+ * Characterizes the Euclidean line between p and q.
+ * @param {NodeObject} p - A point with defined 'x' and 'y'
+ * @param {NodeObject} q - A point with defined 'x' and 'y'
+ * @returns {'a': number, 'b': number, 'c': number}  - Parameters of line in the standard form: ax + by -c = 0
+ */
 function euclid_line(p,q){
-  //Todo: make this more robust to error
+  //TODO: make this more robust to error
   let line =  {'a': p.y-q.y,
           'b': q.x - p.x,
           'c': p.x*q.y - q.x*p.y};
@@ -103,18 +158,36 @@ function euclid_line(p,q){
   }
 }
 
+/**
+ * Returns a line perpendicular to pq that contains v
+ * @param {LineObject} pq - A line with defined 'a' and 'b' and 'c'
+ * @param {NodeObject} v - A point with defined 'x' and 'y'
+ * @returns {'a': number, 'b': number, 'c': number}  - Parameters of line in the standard form: ax + by -c = 0
+ */
 function get_perpendicular_line(pq,v){
   //Returns a line perpendicular to pq that contains v
   return {'a': pq.b, 'b': -pq.a, 'c': -v.x * pq.b + v.y*pq.a};
 }
 
+/**
+ * Returns Euclidean midpoint between p and q
+ * @param {NodeObject} p - A point with defined 'x' and 'y'
+ * @param {NodeObject} q - A point with defined 'x' and 'y'
+ * @returns {'x': number, 'y': number}  - midpoint between p and q
+ */
 function find_midpoint(p,q){
   //Returns the midpoint between p and q
   return {'x': (p.x + q.x) / 2, 'y': (p.y + q.y) / 2};
 }
 
+/**
+ * Finds the point of intersection of lines pq and xy
+ * @param {LineObject} pq - A line with defined 'a','b','c'
+ * @param {LineObject} xy - A line with defined 'a','b','c''
+ * @returns {'x': number, 'y': number}  - Intersection point
+ */
 function find_intersection(pq,xy){
-  //Todo: Handle parallel lines
+  //TODO: Handle parallel lines
   //Returns the point of intersection between lines pq and xy
   return {
     'x': (pq.c * xy.b - pq.b * xy.c) / (pq.b * xy.a - pq.a * xy.b),
@@ -122,17 +195,31 @@ function find_intersection(pq,xy){
   };
 }
 
-//Circle inversion
+/**
+ * Finds the point in the inversion of the unit circle.
+ * See https://en.wikipedia.org/wiki/Inversive_geometry#Inversion_in_a_circle for details
+ * @param {NodeObject} p - A point with defined 'x' and 'y'
+ * @param {CircleObject} circle - A circle with defined 'cx', 'cy', and 'r'
+ * @returns {'x': number, 'y': number}  - Inverted point
+ */
 function circle_inversion(p,circle){
-  //Inverts p about circle, returning the new point p' See link for details:
-  //https://en.wikipedia.org/wiki/Inversive_geometry#Inversion_in_a_circle
-  let dist = euclid_dist(p, circle.center);
+  let cx = circle.cx
+  let cy = circle.cy
+
+  let dist = euclid_dist(p, {'x':cx, 'y': cy});
   let new_c = circle.r*circle.r/(dist*dist);
-  let u = {'x': (p.x - circle.center.x), 'y': (p.y - circle.center.y)};
-  return {'x': new_c * u.x + circle.center.x, 'y': new_c * u.y + circle.center.y};
+  let u = {'x': (p.x - cx), 'y': (p.y - cy)};
+  return {'x': new_c * u.x + cx, 'y': new_c * u.y + cy};
 }
 
-//Hyperbolic geodesic between two points in Poincare disk
+//
+/**
+ * Hyperbolic geodesic between two points in Poincare disk
+ * @param {NodeObject} p - startpoint
+ * @param {NodeObject} q - endpoint
+ * @param {PoinDisk} poindisk - Poindisk object
+ * @returns {'p1': NodeObject, 'p2': NodeObject, 'C': NodeObject, 'r': number, 'startAngle': number, 'endAngle': number}  - Characterization of arc in the poincare disk.
+ */
 exports.poincare_geodesic = function poincare_geodesic(p,q, poindisk){
   //Steps of the algorithm are as follows:
   //Find inverted points outside the unit disk
@@ -140,10 +227,10 @@ exports.poincare_geodesic = function poincare_geodesic(p,q, poindisk){
   //Construct perpendicular lines at midpoint
   //Get intersection of perpendicular lines, C
   //C, along with p and q, fully characterize the arc.
-  let left = poindisk.boundbox.left
-  let top = poindisk.boundbox.top
-  p = {'x': p.x-left, 'y': p.y-top}
-  q = {'x': q.x-left, 'y': q.y-top}
+  //let left = poindisk.boundbox.left
+  //let top = poindisk.boundbox.top
+  //p = {'x': p.x-left, 'y': p.y-top}
+  //q = {'x': q.x-left, 'y': q.y-top}
 
 
   let pp = circle_inversion(p,poindisk);
@@ -160,6 +247,12 @@ exports.poincare_geodesic = function poincare_geodesic(p,q, poindisk){
           'r': euclid_dist(p,C)};
 }
 
+/**
+ * Hyperbolic geodesic between two points in Poincare disk
+ * @param {ArcObject} arc - Object with valid p1, p2, C, r, startAngle, and endAngle attributes
+ * @param {NodeObject} q - PoinDisk
+ * @returns {path}  - Path string for the arc.
+ */
 exports.arc_path = function arc_path(arc, poindisk){
   //Takes an arc object (generated by poincare_geodesic) and
   //return a canvas/svg path for it.
@@ -183,8 +276,15 @@ exports.arc_path = function arc_path(arc, poindisk){
 }
 
 //Circle functions--------------------------------------------------------------
+/**
+ * Generates circle object with the correct center and radius
+ * @param {NodeObject} center - Center in the poincare disk of cricle
+ * @param {number} r - hyperbolic radius of circle
+ * @param {NodeObject} q - PoinDisk
+ * @returns {'cx': number, 'cy': number, 'r': number}  - Circle with the projected cx, cy, and r
+ */
 
-//Todo: Allow for a circle given two/three points
+//TODO: Allow for a circle given two/three points
 function poincare_circle(center,r, poindisk){
   //Return a circle in the poincare disk with center center and hyperbolic radius r
   //Math is done in terms of the Poincare disk
